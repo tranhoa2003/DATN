95183f44163890c9355d0937319b6a3788f7d4a3
Hoà hoa hoè <97334361+tranhoa2003@users.noreply.github.com>
2025-08-30 10:14:22 +0700

    Add files via upload

---
+++ DATN/topcv-backend/utils/sendEmail.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const sendEmail = async (to, subject, text) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: `"TOPCV App 👔" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      text,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log('✅ Email sent:', info.response);
+  } catch (error) {
+    console.error('❌ Error sending email:', error);
+  }
+};
+
+module.exports = sendEmail;
